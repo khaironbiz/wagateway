fd047ff30eda8edb75fd6e653ed28081e15bcc10
khaironbiz <khaironbiz@gmail.com>
2023-06-10 21:49:54 +0700

    paket mengirimkan wa

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 use ay4t\WhatsAppHelper\WhatsAppSG;
 require ('vendor/autoload.php');
 
-$wa     = new WhatsAppSG();
+$wa         = new WhatsAppSG();
 $message    = "Ini adalah latihan mengirim whatapp melalui api gateway";
 $wa->setPort('6789')
     ->setSenderPhone('081213798746')
-    ->setRecepient('081519850159')
-    ->setMessage('test kirim wa?');
+    ->setRecepient('081906011985')
+    ->setMessage($message);
 
 var_dump($wa->SendText());
